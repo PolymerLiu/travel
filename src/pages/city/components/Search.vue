6bad94896f374bfd2f2cc2016172163951c1005e
JeffreyLiu <17771770679@163.com>
2018-12-10 21:52:37 +0800

    城市搜索页面

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,14 +1,61 @@
 <template>
-  <div class="search">
-    <input class="search-input" placeholder="请输入城市名或拼音">
+  <div>
+    <div class="search">
+      <input v-model="keyword" class="search-input" placeholder="请输入城市名或拼音">
+    </div>
+    <div class="search-content" ref="search"  v-show="keyword">
+      <ul>
+        <li class="search-item border-bottom" v-for="item of list" :key="item.id">{{item.name}}</li>
+        <li class="search-item border-bottom" v-show="showNoMatch">没有找到匹配数据</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import Bscroll from "better-scroll";
 export default {
   name: "CitySearch",
+  props: ["cities"],
+  computed:{
+    showNoMatch(){
+      return !this.list.length
+    }
+  },
   data() {
-    return {};
+    return {
+      keyword: "",
+      list: [],
+      timer: null
+    };
+  },
+  mounted() {
+    this.scroll = new Bscroll(this.$refs.search);
+  },
+  watch: {
+    keyword() {
+      if (this.timer) {
+        clearTimeout(this.timer);
+      }
+      if (!this.keyword) {
+        this.list = [];
+        return;
+      }
+      this.timer = setTimeout(() => {
+        const result = [];
+        for (const i in this.cities) {
+          this.cities[i].forEach(element => {
+            if (
+              element.spell.indexOf(this.keyword) > -1 ||
+              element.name.indexOf(this.keyword) > -1
+            ) {
+              result.push(element);
+            }
+          });
+        }
+        this.list = result;
+      }, 100);
+    }
   }
 };
 </script>
@@ -32,5 +79,23 @@ export default {
     color: #666;
     border: none;
   }
+}
+
+.search-content {
+  overflow: hidden;
+  position: absolute;
+  top: 1.58rem;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 1;
+  background: #eee;
+}
+
+.search-item {
+  line-height: 0.62rem;
+  padding-left: 0.2rem;
+  color: #666;
+  background: #fff;
 }
 </style>
